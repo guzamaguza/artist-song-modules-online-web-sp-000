07acc4ddc71a6c7fec327d07077f7debccc8ce70
Mark A Guza <guzamaguza@gmail.com>
2020-02-11 04:32:56 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,13 @@
 module Memorable
-  module reset_all
-    self.all.clear
-  end
 
-  module count
-    self.all.count
+  module ClassMethods
+      module reset_all
+        self.all.clear
+      end
+
+      module count
+        self.all.count
+      end
   end
 
 end
